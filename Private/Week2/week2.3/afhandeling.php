714b97d76f14fc16e4b51a9df5b284540a3102dc
Kjell <34952@ma-web.nl>
2023-06-01 12:24:42 +0200

    week3

---
+++ Private/Week2/week2.3/afhandeling.php
@@ -0,0 +1,7 @@
+<?php
+    var_dump($_GET);
+    $kaas = "kaas";
+    echo $kaas;
+    echo $_GET ["email"];
+    echo $_GET ["naam"];
+?>
---
+++ Private/week3 opdracht1/index.php
@@ -0,0 +1,29 @@
+<?php
+
+    function lijst(){
+    $getallen = array(2, 3, 4, 5, 6);
+    $short_syntax = [2, 3, 4, 5, 6];
+
+    echo $getallen[0];
+    echo $getallen[1];
+    echo $getallen[2];
+    echo $getallen[3];
+    echo $getallen[4];
+}
+
+lijst();
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
